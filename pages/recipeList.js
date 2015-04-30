2b6de4ab4cf4f3a2608e2fd6ed4f2527e33d7575
Siahn <ti.siahn@gmail.com>
2015-04-30 14:58:25 -0700

    got rid of old images

--- pages/recipeList.js
+++ pages/recipeList.js
@@ -7,13 +7,13 @@ function initXHR(x) {
 }
 
 function retrieveActiveListsFromServer(url) {
-    
+
 	var xmlhttp = new XMLHttpRequest();
 	var list;
 
 	xmlhttp.onreadystatechange = function() {
-        
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {            
+
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 			var list = JSON.parse(xmlhttp.responseText);
             console.log('after JSON.parse');
 			populateListsView('list', list);
@@ -37,7 +37,6 @@ function populateListsView(elementId, list) {
         newElement += "<img class=\"img-responsive\" src=\"" + list[i].image +  "\" alt=\"\">";
         newElement += "<p></p>";
         newElement += "<span class=\"glyphicon glyphicon-edit\"></span>";
-        newElement += "<span class=\"glyphicon glyphicon-remove-sign\"></span>";
         newElement += "<span class=\"glyphicon glyphicon-trash\"></span>";
         newElement += "</a>";
         newElement += "</div>";
@@ -45,14 +44,14 @@ function populateListsView(elementId, list) {
         newElement += "<h4>" + list[i].name + "</h4>";
         newElement += "<h5>Description</h5>";
         newElement += "<p>" + list[i].description + "</p>";
-        newElement += "<a class=\"btn btn-primary\" href=\"recipeView.html\">View Full Recipe <span class=\"glyphicon glyphicon-chevron-right\"></span></a>";
+        newElement += "<a class=\"btn btn-xl\" href=\"recipeView.html\">View Full Recipe <span class=\"glyphicon glyphicon-chevron-right\"></span></a>";
         newElement += "</div>";
         newElement += "<div class=\"col-md-3\">";
         newElement += "<h4>Shared With</h4>";
         for (var j = 0; j < list[i].sharedWith.length; j++) {
             newElement += "<li>" + list[i].sharedWith[j] + "</li>";
         }
-        newElement += "<p><span class=\"glyphicon glyphicon-plus-sign\"></span></a></p>";
+        newElement += "<a><span class=\"glyphicon glyphicon-plus-sign\"></span></a>";
         newElement += "</div>";
         newElement += "</div>";
         newElement += "<hr>";
